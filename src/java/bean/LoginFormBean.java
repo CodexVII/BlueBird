7073580b6b9e15b421b0b8e12ba5578ba395f672
Ian <keitaro_ayanokoji@yahoo.ie>
2017-03-23 16:14:55 +0000

    Login through database and security servlet implemented

---
+++ src/java/bean/LoginFormBean.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import java.io.IOException;
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author keita
+ */
+@ManagedBean
+@SessionScoped
+public class LoginFormBean implements Serializable {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+
+        // attempt to login
+        // redirect if successful
+        try {
+            // successful login
+            request.login(username, password);
+            return "/home.xhtml?faces-redirect=true";
+        } catch (ServletException e) {
+            // failed login
+            FacesMessage msg = new FacesMessage("Failed to log user in");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            context.addMessage(null, msg);
+            return "/loginerror.xhtml?faces-redirect=true";
+        }
+    }
+
+    public void logout() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        HttpServletRequest request = (HttpServletRequest) context.getExternalContext().getRequest();
+        try {
+            request.logout();
+            FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+        } catch (ServletException e) {
+            FacesMessage msg = new FacesMessage("Failed to logout");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            context.addMessage(null, msg);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
