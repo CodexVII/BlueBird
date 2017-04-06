1634e18f4483b39fb78a358a4b1369dfb7ea812b
Gearóid <gearoidc@hotmail.co.uk>
2017-04-06 01:25:19 +0100

    Product Browsing Quantity Support
    -Updated product datatype to support quantity
    -Update shopping page to reflect this
    -ToDo: Because JSF is great, I can't bind the quanttiy input to the
    quattity variable. So we need to get some shit sorted

--- BlueBird-war/src/java/information_tracking/products.java
+++ BlueBird-war/src/java/information_tracking/products.java
@@ -28,56 +28,112 @@ public class products implements Serializable {
     /**
      * Creates a new instance of products
      */
+    public class ShoppingItem{
+        private Product p;
+        private int quantity;
+
+        public ShoppingItem(Product p, int quantity)
+        {
+            this.p = p;
+            this.quantity = quantity;
+        }        
+        public Product getP() {
+            return p;
+        }
+
+        public void setP(Product p) {
+            this.p = p;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+        
+        
+    }
+    private List<ShoppingItem> shoppingList;
     
-    private List<Product> shoppingList;
-  
+    private Boolean administrator;
     
  // <editor-fold desc="Setter and Getter Garbage.">
 
     @Inject
     UserEJB usr;
     
-    public List<Product> getAllProducts() {
-        return usr.getAllProducts();
+    public List<ShoppingItem> getAllProducts() {
+        List<Product> ejbProds = usr.getAllProducts();
+        List<ShoppingItem> s1 = new ArrayList<ShoppingItem>();
+        for(int i =0; i < ejbProds.size(); i++){
+            s1.add(new ShoppingItem(ejbProds.get(i),0));
+        }
+        return s1;
+        
     }
 
-    public List<Product> getShoppingList() {
+    public List<ShoppingItem> getShoppingList() {
         return shoppingList;
     }
 
-    public void setShoppingList(List<Product> shoppingList) {
+    public void setShoppingList(List<ShoppingItem> shoppingList) {
         this.shoppingList = shoppingList;
     }
-    
+
+    public Boolean getAdministrator() {
+        return administrator;
+    }
+
+    public void setAdministrator(Boolean administrator) {
+        this.administrator = administrator;
+    }
+
+    public UserEJB getUsr() {
+        return usr;
+    }
+
+    public void setUsr(UserEJB usr) {
+        this.usr = usr;
+    }
+
     
 // </editor-fold>
     
     public products() {
-        shoppingList = new ArrayList<Product>();
-
+        this.shoppingList = new ArrayList<ShoppingItem>();
+        this.administrator = true;
     }
     
-    public void addProductRow(int index, String name, String des, int stock, double price){
+    public void addProductRow( String name, String des, int stock, double price){
         
     }
     
-    
-    public void removeFromBasket(Product p){
+    public void removeFromBasket(ShoppingItem p){
         this.shoppingList.remove(p);
     }
     
-    public void addToBasket(Product p){
+    public void addToBasket(ShoppingItem p){
         if(!shoppingList.contains(p)){
-            System.out.println("Adding item to basket Item " + p.getName());
+            System.out.println("Adding "+ p.quantity +" of item to basket: " + p.p.getName());
             this.shoppingList.add(p);
         }
         else{
-            System.out.println("Item already present " + p.getName());
+            System.out.println("Item already present " + p.p.getName());
         }
     }
     
      public void changeItem(Product p) {
-        
+        System.out.println("EJB will change: " + p.getName());
+    }
+     
+    public double getShoppingTotal(){
+        double total=0.0;
+        for(int i =0; i<this.shoppingList.size();i++){
+            total+= this.shoppingList.get(i).p.getPrice() * this.shoppingList.get(i).quantity;
+        }
+        return total;
     }
     
 }
