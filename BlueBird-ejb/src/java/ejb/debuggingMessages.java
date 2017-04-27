7a7a5bddb57c26fae27f43ae74aed2719bf2ae08
Gearóid <gearoidc@hotmail.co.uk>
2017-04-27 15:09:04 +0100

    Added log file to MDB

--- BlueBird-ejb/src/java/ejb/debuggingMessages.java
+++ BlueBird-ejb/src/java/ejb/debuggingMessages.java
@@ -5,6 +5,10 @@
  */
 package ejb;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.ejb.ActivationConfigProperty;
 import javax.ejb.MessageDriven;
 import javax.jms.Message;
@@ -19,15 +23,23 @@ import javax.jms.MessageListener;
     ,
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue")
 })
-public class debuggingMessages implements MessageListener {
-    
-    public debuggingMessages() {
+public class DebuggingMessages implements MessageListener {
+    FileOutputStream f1;
+    public DebuggingMessages() {
+        try{
+           this.f1 = new FileOutputStream("log.txt", true);
+        }catch(FileNotFoundException e){
+            System.err.println("MDB File Error: " + e.toString());
+        }
     }
     
     @Override
     public void onMessage(Message message) {
         try{
-        System.out.println("Received from queue: " + message.getStringProperty("log"));
+        String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+        String toSend = timeStamp +": " + message.getStringProperty("log") + "\n";
+        this.f1.write(toSend.getBytes());
+        this.f1.flush();
         }catch(Exception QueueError){
             System.out.println("Error received from message queue: " + QueueError.toString());
         }
